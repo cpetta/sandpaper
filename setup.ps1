9a38bb2445518d8a2eb7650ee7ae566309607ee1
Chris <cpetta@oaisd.org>
2019-02-05 10:54:38 -0500

    Moved sections into functions

--- setup.ps1
+++ setup.ps1
@@ -61,89 +61,105 @@ $npmSaveDevPackages = @(
 
 $NPMPath = "C:\Program Files\nodejs"
 $GITPath = "C:\Program Files\Git\cmd"
-
-#Determine if updating or setting up for the first time.
-{
-	$scriptname = $MyInvocation.MyCommand.Name;
+$scriptname = $MyInvocation.MyCommand.Name
+$SettingUp
+# Determine if updating or setting up for the first time.
 	if($scriptname -eq "setup.ps1") { $SettingUp = "true" }
 	if($scriptname -eq "update.ps1") { $Updating = "true" }
 	if(($SettingUp -ne "true") -and ($Updating -ne "true"))
 	{
+		Write-Output "$scriptname" 
 		Write-Output "Unable to detirmine if updating or setting up. Please make sure this powershell script is named update.ps1 or setup.ps1"
+		return
+	}
+
+# Install/Update Chocolatey
+function runChocolatey { 
+	Write-Output "Checking to see if Chocolatey is installed..."
+
+	if (Get-Command choco -errorAction SilentlyContinue) {
+		Write-Output "Chocolatey is already installed"
+		choco.exe upgrade chocolatey -y
+		
+		Write-Output "Attempting to update apps using chocolatey"
+		choco.exe upgrade $Applications -y
+	}
+	else {
+		Write-Output "Chocolatey isn't installed, attempting to install. "
+		Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+		Write-Output "Attempting to install apps with chocolatey"
+		choco install $Applications -y
+	}
+}
+
+# Download most uptodate files from github and initialize git.
+function runGit {
+	if(-not ((Get-Command git -errorAction SilentlyContinue))) {
+		$Env:Path = "$Env:Path;$GITPath"
+	}
+
+	if (Get-Command git -errorAction SilentlyContinue) {
+		git init
+		git remote add gulpdev https://github.com/cpetta/GulpDev.git
+		git fetch
+		git pull gulpdev master
+	}
+	else {
+		Write-Output "GIT command wasn't found, there may have been a problem during installation, try running this script again."
 		return
 	}
 }
 
-Write-Output "Checking to see if Chocolatey is installed..."
-
-if (Get-Command choco -errorAction SilentlyContinue) {
-	Write-Output "Chocolatey is already installed"
-	choco.exe upgrade chocolatey -y
-	
-	Write-Output "Attempting to update apps using chocolatey"
-	choco.exe upgrade $Applications -y
-}
-else {
-	Write-Output "Chocolatey isn't installed, attempting to install. "
-	Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-	Write-Output "Attempting to install apps with chocolatey"
-	choco install $Applications -y
-}
-
-if(-not ((Get-Command npm -errorAction SilentlyContinue))) {
-	$Env:Path = "$Env:Path;$NPMPath"
-}
-
-if (Get-Command npm -errorAction SilentlyContinue) {
-	Write-Output "Installing NPM packages..."
-
-#	PreOP
-	npm --silent install  npm@latest -g
-	npm --silent install node@latest -g --scripts-prepend-node-path
-	if ($SettingUp -eq "true")
-	{
-		npm --silent init -f
-		npm --silent rm --global gulp
-		npm --silent cache clean -f
+# Install/Update npm packages and initialize project.
+function runNpm{
+	if(-not ((Get-Command npm -errorAction SilentlyContinue))) {
+		$Env:Path = "$Env:Path;$NPMPath"
 	}
 
-#	install/update packages
-	npm --silent install -g $npmGlobalPackages
-	npm --silent install -save-dev $npmSaveDevPackages
+	if (Get-Command npm -errorAction SilentlyContinue) {
+		Write-Output "Installing NPM packages..."
 
-#	PostOP
-	npm --silent audit fix	
-	
+	#	PreOP
+		npm --silent install  npm@latest -g
+		npm --silent install node@latest -g --scripts-prepend-node-path
+		if ($SettingUp -eq "true")
+		{
+			npm --silent init -f
+			npm --silent rm --global gulp
+			npm --silent cache clean -f
+		}
+
+	#	install/update packages
+		npm --silent install -g $npmGlobalPackages
+		npm --silent install -save-dev $npmSaveDevPackages
+
+	#	PostOP
+		npm --silent audit fix	
+		
+		if($SettingUp -eq "true")
+		{
+			tslint --init
+		}
+	}
+	else {
+		Write-Output "NPM command wasn't found, there may have been a problem during installation, try running this script again."
+		return
+	}
+}
+
+# Finalization
+function finalizeSetup{
 	if($SettingUp -eq "true")
 	{
-		tslint --init
+		$path = Get-Location
+		copy-item "$path\setup.ps1" -Destination "$path\update.ps1"
+		Set-Location dev
+		Start-Process index.html
 	}
 }
-else {
-	Write-Output "NPM command wasn't found, there may have been a problem during installation, try running this script again."
-	return
-}
 
-if(-not ((Get-Command git -errorAction SilentlyContinue))) {
-	$Env:Path = "$Env:Path;$GITPath"
-}
-
-if (Get-Command git -errorAction SilentlyContinue) {
-	git init
-	git remote add gulpdev https://github.com/cpetta/GulpDev.git
-	git fetch
-	git pull gulpdev master
-	Set-Location dev
-	Start-Process index.html
-}
-else {
-	Write-Output "GIT command wasn't found, there may have been a problem during installation, try running this script again."
-	return
-}
-
-if($SettingUp -eq "true")
-{
-	Set-Location ..
-	try { rename-item "setup.ps1" "update.ps1" } catch {}
-}
+runChocolatey
+runGit
+runNpm
+finalizeSetup
