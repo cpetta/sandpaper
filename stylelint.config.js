2fd8b28c9a6b9392c306088bc301f2c2243f889e
Chris <cpetta@oaisd.org>
2019-02-19 16:03:45 -0500

    added proper stylelint config

---
+++ stylelint.config.js
@@ -0,0 +1,16 @@
+"use strict"
+
+module.exports = {
+  "extends": "stylelint-config-standard",
+  "plugins": [
+		"stylelint-order"
+	],
+  "rules": {
+	"order/order": [
+		"custom-properties",
+		"declarations"
+	],
+	"order/properties-alphabetical-order": true,
+    "indentation": "tab",
+  },
+}
